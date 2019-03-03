925ef433c6db63dbf96431b4f5c35024a9ddeda0
Matthias Herrmann <matthias.mh.herrmann@googlemail.com>
2019-03-03 19:34:07 +0100

    Playing with van laarhoven lenses

--- shell.nix
+++ shell.nix
@@ -1,5 +1,33 @@
-let pkgs = import <nixpkgs> {};
-    haskellPkgs = pkgs.haskellPackages;
+let pkgs =
+  let localPkgs = import <nixpkgs> {};
+  in import (localPkgs.fetchFromGitHub {
+    owner = "NixOS";
+    repo = "nixpkgs";
+    rev = "91c7157919081c5bbc8cb808e27d89ed4a8cbef7";
+    sha256 = "0hqscxpkgnpvm8354sxf3vi7j6chzv0dsy3g8pgmhfacha4hfwl5";
+  }) {};
+  myHaskTags = pkgs.haskell.lib.dontCheck haskellPkgs.hasktags;
+  haskellPkgs = pkgs.haskell.packages.ghc863;
+  hiePkgs = import (pkgs.fetchFromGitHub {
+    owner = "domenkozar";
+    repo = "hie-nix";
+    rev = "6794005f909600679d0b7894d0e7140985920775";
+    sha256 = "0pc90ns0xcsa6b630d8kkq5zg8yzszbgd7qmnylkqpa0l58zvnpn";
+  }){};
 in {
-  myHaskellStackEnv = pkgs.myHaskellStackEnv;
+  scrapeChangesStackEnv = pkgs.haskell.lib.buildStackProject {
+    name = "rawtherapeeProcStackEnv";
+    buildInputs = with haskellPkgs; [
+      hiePkgs.hie86
+      stack
+      pkgs.zlib
+      myHaskTags
+      hoogle
+      #brittany
+      /*hdevtools*/ /*ghcid*/ #ghc-mod
+      pkgs.fzf
+    ];
+    ghc = haskellPkgs.ghc;
+    #HIE_HOOGLE_DATABASE=./.stack-work/hoogle/x86_64-linux-nix/lts-12.26/8.4.4/database.hoo;
+  };
 }
--- src/Manual.hs
+++ src/Manual.hs
@@ -1,3 +1,10 @@
+{-# LANGUAGE RankNTypes #-}
+
+module Manual where
+
+import Control.Applicative (getConst, Const(..))
+import Data.Functor.Identity (Identity(..), runIdentity)
+
 data Lens a b = Lens {get :: a -> b, set :: b -> a -> a}
 
 compose :: Lens a b -> Lens b c -> Lens a c
@@ -9,7 +16,7 @@ data Address = Address {street :: String, zip :: Integer} deriving (Show)
 
 data Student = Student {studentname :: String, address :: Address} deriving (Show)
 
-data Professor = Professor {professorname :: String} deriving (Show)
+newtype Professor = Professor {professorname :: String} deriving (Show)
 
 data Coach = Coach {topic :: String, prof :: Professor, student :: Student} deriving (Show)
 
@@ -21,3 +28,17 @@ studentToAddress = Lens address (\a s -> Student (studentname s) a)
 
 coachToAddress :: Lens Coach Address
 coachToAddress = coachToStudent |.| studentToAddress
+
+type VLLens a b = forall f . Functor f => (b -> f b) -> a -> f a
+
+getVL :: VLLens a b -> a -> b
+getVL lens = getConst . lens Const
+
+setVL :: VLLens a b -> b -> a -> a
+setVL lens b a =
+  runIdentity $ lens (const $ Identity b) a
+
+coachToStudentVL :: VLLens Coach Student
+coachToStudentVL setter coach =
+  fmap (\student' -> coach { student = student'}) (setter (student coach))
+
