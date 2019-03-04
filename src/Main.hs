6b5331ebc02b0252015c29032394713a63b6c5c5
Matthias Herrmann <matthias.mh.herrmann@googlemail.com>
2019-03-04 18:10:11 +0100

    Make full use of duplicate record fields

--- src/Main.hs
+++ src/Main.hs
@@ -1,79 +1,89 @@
-{-# LANGUAGE TemplateHaskell, FlexibleContexts #-}
+{-# LANGUAGE TemplateHaskell, FlexibleContexts, DuplicateRecordFields, TypeApplications #-}
 
 module Main where
-import Control.Lens
-import Data.Monoid ((<>))
-import Data.Maybe (isJust)
+import           Control.Lens
+import           Data.Monoid                    ( (<>) )
+import           Data.Maybe                     ( isJust )
 
 data Gender = Male | Female deriving (Eq, Show)
 
-data Course = Course {
-  _couName :: String
+newtype Course = Course {
+  name :: String
 } deriving (Eq, Show)
-
-makeLenses ''Course
 
 data Student = Student {
-  _stuName :: String,
-  _stuCourses :: [Course],
-  _stuGender :: Gender
+  name :: String,
+  courses :: [Course],
+  gender :: Gender
 } deriving (Eq, Show)
 
-makeLenses ''Student
+makeLensesFor [
+    ("courses", "_stuCourses")
+  , ("gender", "_stuGender")
+  , ("name", "_stuName")
+    ] ''Student
 
 data Prof = Prof {
-  _profName :: String,
-  _profCourses :: [Course]
+  name :: String,
+  courses :: [Course]
 } deriving (Eq, Show)
 
-makeLenses ''Prof
-
 traverseCourses :: Traversal' Student Course
-traverseCourses = stuCourses . each
+traverseCourses = _stuCourses . each
 
+studentsOfProfTraversal
+  :: (Each s t Student Student, Applicative f)
+  => Prof
+  -> (Student -> f Student)
+  -> s
+  -> f t
 studentsOfProfTraversal prof =
-  let profCourses = _profCourses prof
-  in each . filtered (isJust . findOf (stuCourses . each) (`elem` profCourses))
+  --let profCourses' = (courses :: Prof -> [Course]) prof
+  let profCourses' = courses (prof :: Prof)
+  in  each . filtered
+        (isJust . findOf (_stuCourses . each) (`elem` profCourses'))
 
 studentsOfProf :: Prof -> [Student] -> [Student]
 studentsOfProf prof = toListOf (studentsOfProfTraversal prof)
 
 main :: IO ()
 main =
-  let courses = Course <$> ["a", "b", "c"]
-      mat = Student {
-        _stuName = "Matthias",
-        _stuCourses = courses,
-        _stuGender = Male
-      }
-      tina = Student {
-        _stuName = "Tina",
-        _stuCourses = Course <$> ["x", "e"],
-        _stuGender = Female
-      }
-      bernd = Student {
-        _stuName = "Bernd",
-        _stuCourses = Course <$> ["a", "e"],
-        _stuGender = Male
-      }
-      gust = Student {
-        _stuName = "Gustav",
-        _stuCourses = Course <$> ["b", "e"],
-        _stuGender = Male
-      }
-      prof = Prof {
-        _profName = "Prof. X",
-        _profCourses = Course <$> ["d", "e"]
-      }
-  in do
-   print mat
-   let students = [mat, bernd, gust, tina]
-   putStrLn $ "Students of prof: " <> show (studentsOfProf prof students)
-   putStrLn "Adding a course to all students of prof"
-   let added = over (studentsOfProfTraversal prof . stuCourses) (<> [Course "new one"]) students
-   putStrLn $ "Result: " <> show added
-   let added2 = students & (studentsOfProfTraversal prof . stuCourses) %~ (<> [Course "new one"])
-   putStrLn $ "Inlined: " <> show added2
-   putStrLn "Adding a postfix to the name of all male students of prof"
-   let postfixAdded = students & over (studentsOfProfTraversal prof . filtered (\student -> Male == view stuGender student) . stuName) (<> "!MALE!")
-   print $ show postfixAdded
+  let
+    courses' = Course <$> ["a", "b", "c"]
+    mat      = Student { name = "Matthias", courses = courses', gender = Male }
+    tina     = Student { name    = "Tina"
+                       , courses = Course <$> ["x", "e"]
+                       , gender  = Female
+                       }
+    bernd = Student { name    = "Bernd"
+                    , courses = Course <$> ["a", "e"]
+                    , gender  = Male
+                    }
+    gust = Student { name    = "Gustav"
+                   , courses = Course <$> ["b", "e"]
+                   , gender  = Male
+                   }
+    prof = Prof { name = "Prof. X", courses = Course <$> ["d", "e"] }
+  in
+    do
+      print mat
+      let students = [mat, bernd, gust, tina]
+      putStrLn $ "Students of prof: " <> show (studentsOfProf prof students)
+      putStrLn "Adding a course to all students of prof"
+      let added = over (studentsOfProfTraversal prof . _stuCourses)
+                       (<> [Course "new one"])
+                       students
+      putStrLn $ "Result: " <> show added
+      let added2 =
+            students
+              &  (studentsOfProfTraversal prof . _stuCourses)
+              %~ (<> [Course "new one"])
+      putStrLn $ "Inlined: " <> show added2
+      putStrLn "Adding a postfix to the name of all male students of prof"
+      let postfixAdded = students & over
+            ( studentsOfProfTraversal prof
+            . filtered (\student -> Male == view _stuGender student)
+            . _stuName
+            )
+            (<> "!MALE!")
+      print $ show postfixAdded
